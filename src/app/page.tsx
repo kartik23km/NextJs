c7829d2f56ecae6b87aa79ef855dc1f3b369b6bf
kartik23km <malhotrakartik5@gmail.com>
2025-10-05 03:39:01 +0530

    Added Link tags in NextJs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello World from next js.</div>;
+  return (
+    <div className="m-5 text-2xl flex flex-col">
+      <Link href={"/performance"}>Performance</Link>
+      <Link href={"/reliability"}>Reliability</Link>
+      <Link href={"/scale"}>Scale</Link>
+    </div>
+  );
 }
