db03f33204736ae15fc01cdc29a81ad71e7d57fc
kartik23km <malhotrakartik5@gmail.com>
2025-10-07 02:35:40 +0530

    understanding layout component  in NextJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <div className="m-5 text-2xl flex flex-col">
-      <Link href={"/performance"}>Performance</Link>
-      <Link href={"/reliability"}>Reliability</Link>
-      <Link href={"/scale"}>Scale</Link>
-    </div>
-  );
+  return <div>This is Home page</div>;
 }
